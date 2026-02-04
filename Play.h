fafbc32c98ceeb78980e1d4feacff6dbabeb2988
Laura Pazini Medeiros <laurapazinimedeiros@gmail.com>
2026-02-04 19:54:43 -0300

    feat: added a header file to Play.cpp

---
+++ Play.h
@@ -0,0 +1,67 @@
+#ifndef PLAY_H
+    #define PLAY_H
+    #include "Board.h"
+    #include "Bot.cpp"
+    #include "Optimal_algorithm.cpp"
+    using namespace std;
+
+    /**
+     * @class TicTacToeMiniMax
+     * @brief Runs a game between a learning bot and an optimal algorithm
+     */
+    class TicTacToeMiniMax{
+        private:
+        bool curr_player; // 0 = P1, 1 = P2
+        BOARD board;        
+        BOT* bot_ref;
+        Optimal_algorithm* minimax_ref;        
+        char P1_SYMBOL; // Starting player's symbol
+        char P2_SYMBOL; // Second player's symbol
+
+        void switch_player(void);
+
+        public:
+        TicTacToeMiniMax(BOT& bot, Optimal_algorithm& minimax);
+        short run_game(bool bot_is_x, const bool& print = true);
+    };
+
+    /**
+     * @class TicTacToeBOT
+     * @brief Has two bots compete against themselves.
+     */
+    class TicTacToeBOT {
+        private:
+        bool curr_player; // 0 = p0, 1 = p1
+        BOARD board;
+
+        void TicTacToeBOT::switch_player(void);
+
+        public:
+        array<BOT, 2> players; // Stores each player (BOT)
+
+        TicTacToeBOT(BOT& X, BOT& O);
+        short botVSbot(const bool& print = true);
+    };
+
+    /**
+     * @class TicTacToePlayer
+     * @brief Runs a game between a human player and a bot.
+     */
+    class TicTacToePlayer{
+        private:
+        bool curr_player; // 0 = P1, 1 = P2
+        BOARD board;
+        
+        BOT* bot_ref;
+        
+        char P1_SYMBOL; // Símbolo de quem começa
+        char P2_SYMBOL; // Símbolo de quem joga em segundo
+
+        void switch_player(void);
+    
+        public:
+        TicTacToePlayer(BOT& bot);
+        short run_game(bool bot_is_x, const bool& print = true);
+    };
+
+#endif
